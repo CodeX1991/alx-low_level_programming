2f334edcd7105fd619bc95be4c0aff22ca89c027
CodeX1991 <oghenewonaholy@gmail.com>
2022-09-12 03:21:01 -0700

    You are not allowed to use any variable of type char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * Main - Pirnt out number 0 to 9 without char type
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	unsigned int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
