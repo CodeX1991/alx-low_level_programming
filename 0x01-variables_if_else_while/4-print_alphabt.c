5f9088cd62feaa0774a165ad8475cec3baa7c094
CodeX1991 <oghenewonaholy@gmail.com>
2022-09-12 02:43:13 -0700

    Print all the letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,13 @@
 
 int main(void)
 {
-	char alpha_except_q_and_e;
+	char except_q_and_e;
 
-	for (alpha_except_q_and_e = 'a'; alpha_except_q_and_e <= 'z'; alpha_except_q_and_e++)
+	for (except_q_and_e = 'a'; except_q_and_e <= 'z'; except_q_and_e++)
 	{
-		if (alpha_except_q_and_e != 'q' && alpha_except_q_and_e != 'e')
+		if (except_q_and_e != 'q' && except_q_and_e != 'e')
 		{
-			putchar(alpha_except_q_and_e);
+			putchar(except_q_and_e);
 		}
 	}
 	putchar('\n');
