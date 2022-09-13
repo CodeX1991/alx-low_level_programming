cc8566f5f0d5fb7a1e7f4288a8d39affa3a2a658
CodeX1991 <oghenewonaholy@gmail.com>
2022-09-13 06:57:34 -0700

    Update on _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.h"
+
+/**
+ * main - This program prints out _putchar to the  stdout
+ * Return: The value is 0
+ */
+
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
