22307ac86b9b2ce3a3b76453679e33b1323fbc28
CodeX1991 <oghenewonaholy@gmail.com>
2022-09-15 06:56:47 -0700

    Updating  a function that prints the 9 times table, starting with 0.

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 timestable, starting with 0.
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int num1, num2, result;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			result = num1 * num2;
+			
+			if (result > 9)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+				if (num2 == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (result == 0 || result <= 9)
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+				if (num2 == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
