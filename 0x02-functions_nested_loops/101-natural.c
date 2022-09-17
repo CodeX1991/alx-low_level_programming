3d026d2f59c286f63431b2b4a2a567c6c0917b40
CodeX1991 <oghenewonaholy@gmail.com>
2022-09-17 05:30:35 -0700

    Update he sum of these multiples is 23. Write a program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints out the sum of the multples of 3 or below 1024
+ *
+ * Return: sum
+ *
+ */
+
+int main(void)
+{
+	int mult, sum = 0;
+
+	for (mult = 3; mult < 1024; mult++)
+	{
+		if (mult % 3 == 0 || mult % 5 == 0)
+		{
+			sum = sum + mult;
+		}
+	}
+	
+	printf("%d\n", sum);
+	return (sum);
+}
