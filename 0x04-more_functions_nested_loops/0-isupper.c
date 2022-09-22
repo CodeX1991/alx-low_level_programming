71a97084e2b6fd45ebceb2bb28db879114aba488
CodeX1991 <oghenewonaholy@gmail.com>
2022-09-22 09:29:43 -0700

    Update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,16 +10,8 @@
 
 int _isupper(int c)
 {
-	int result;
-
 	if (c >= 'A' && c <= 'Z')
-	{
-	      result = 1;
-	}
+		return (1);
 	else
-	{
-		result = 0;
-	}
-	
-	return (result);
+		return (0);
 }
