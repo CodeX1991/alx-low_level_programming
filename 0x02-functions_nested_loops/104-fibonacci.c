ad882ccd0e96a141b228dcc533249dfab2b9accc
CodeX1991 <oghenewonaholy@gmail.com>
2022-09-22 07:54:17 -0700

    Updating

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,15 +8,40 @@
 int main(void)
 {
 	unsigned long fbn1 = 0, fbn2 = 1, sum;
+	unsigned long fbn1_half1, fbn1_half2, fbn2_half1, fbn2_half2;
+	unsigned long half1, half2;
 	int count;
 
-	for (count = 0; count < 98; count++)
+	for (count = 0; count < 92; count++)
 	{
 		sum = fbn1 + fbn2;
 		printf("%lu, ", sum);
 
 		fbn1 = fbn2;
 		fbn2 = sum;
+	}
+	fbn1_half1 = fbn1 / 10000000000;
+	fbn2_half1 = fbn2 / 10000000000;
+	fbn1_half2 = fbn1 % 10000000000;
+	fbn2_half2 = fbn2 % 10000000000;
+
+	for (count = 93; count < 99; count++)
+	{
+		half1 = fbn1_half1 + fbn2_half1;
+		half2 = fbn1_half2 + fbn2_half2;
+		if (fbn1_half2 + fbn2_half2 > 9999999999)
+		{
+			half1 = half + 1;
+			half2 = half % 10000000000;
+		}
+		printf("%lu%lu", half1, half2);
+		if (count != 98)
+			printf(", ");
+		
+		fbn1_half1 = fbn2_half1;
+		fbn1_half2 = fbn2_half2;
+		fbn2_half1 = half1;
+		fbn2_half2 = half2;
 	}
 	printf("\n");
 	return (0);
