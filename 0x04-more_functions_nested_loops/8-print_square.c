fa1dd0da36f9be8837e97cf2f4bcc8c84c8bb1c8
CodeX1991 <oghenewonaholy@gmail.com>
2022-09-23 09:09:18 -0700

    Update on square function

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square
+ * @size: Size of the square
+ *
+ */
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int counter1, counter2;
+
+		for (counter1 = 1; counter1 <= size; counter1++)
+		{
+			for (counter2 = 1; counter2 <= size; counter2++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+	
+}
