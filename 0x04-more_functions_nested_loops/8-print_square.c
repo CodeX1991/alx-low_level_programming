5f3bb1b347507e626968d50f6bb0e8b00bc95679
CodeX1991 <oghenewonaholy@gmail.com>
2022-09-23 09:21:38 -0700

    Update

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,12 +14,10 @@ void print_square(int size)
 		for (counter1 = 1; counter1 <= size; counter1++)
 		{
 			for (counter2 = 1; counter2 <= size; counter2++)
-			{
 				_putchar('#');
-			}
 			_putchar('\n');
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 	
 }
