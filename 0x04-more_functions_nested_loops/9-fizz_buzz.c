4df221d04907a285f59430d2dacafe948edbfb6e
CodeX1991 <oghenewonaholy@gmail.com>
2022-10-01 14:12:03 -0700

    Updating

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,18 +15,18 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 15 == 0)
-			printf("FizzBzz");
+			printf("FizzBuzz");
 		else if (num % 3 == 0)
 			printf("Fizz");
 		else if (num % 5 == 0)
 			printf("Buzz");
 		else
 			printf("%d", num);
+
 		if (num == 100)
 			continue;
 		printf(" ");
 	}
 	printf("\n");
-
 	return (0);
 }
