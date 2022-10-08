7f9e9d7affb293f87df3331720581319f8a2144f
CodeX1991 <oghenewonaholy@gmail.com>
2022-10-07 17:07:50 -0700

    Update on the commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,29 +12,10 @@ void print_number(int n)
 	{
 		_putchar('-');
 		num = -num;
-		_putchar((num / 10) + '0');
-		_putchar((num % 10) + '0');
 	}
-	else if (n == 0)
+	if ((num / 10) > 0)
 	{
-		_putchar(0 + '0');
+		print_number(num / 10);
 	}
-	else if (n > 9 && n < 100)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n >= 100 && n < 1000)
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n % 100) / 10) + '0');
-		_putchar(((n % 100) % 10) + '0');
-	}
-	else if (n >= 1000 && n < 10000)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n % 1000) / 100) + '0');
-		_putchar((((n % 1000) % 100) / 10) + '0');
-		_putchar((((n % 1000) % 100) % 10) + '0');
-	}
+	_putchar((num % 10) + '0');
 }
