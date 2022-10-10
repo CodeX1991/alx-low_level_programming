b3f9d082ec81056fc64317b1084c5692f2e1e225
CodeX1991 <oghenewonaholy@gmail.com>
2022-10-10 03:47:49 -0700

    Add the structure of a dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+Learning about the use of typedef under the project 0x0E. Here we learn about structures in C programming language
+dog.h: Defining a new type of struct dog
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog attributes
+ * @name: The name of the dpg
+ * @age: The age of the dog
+ * @owner: Name of the owner of the dog
+ *
+ * Description: The attributes of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
