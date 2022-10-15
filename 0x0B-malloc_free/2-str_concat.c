b206659c9de694495972c2c579a2ef2ce488067b
CodeX1991 <oghenewonaholy@gmail.com>
2022-10-15 06:10:10 -0700

    Add 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - A function that concatenates two strings.
+ * @s1: First string
+ * @s2: second string
+ *
+ * Return:NULL on failure, empty string if NULL is passed
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, len = 0;
+	char *concat_str;
+
+	if (s1 == NULL)
+		return (" ");
+	if (s2 == NULL)
+		return (" ");
+
+	for (i = 0; s1[i] != '\0'; i++)
+		len++;
+	for (i = 0; s2[i] != '\0'; i++)
+		len++;
+
+	concat_str = malloc(sizeof(char) * len);
+	if (concat_str == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		concat_str[i] = s1[i];
+
+	for (j = 0; s2[j] != '\0'; j++)
+		concat_str[i++] = s2[j];
+
+	concat_str[++i] = '\0';
+	return (concat_str);
+
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,4 @@
 Learning about malloc in c
 0-create_array.c: A function that creates an array of chars, and inintializes it with a specific char
 1-setrup.c: function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+2-str_concat.c: A function that concatenates two string
