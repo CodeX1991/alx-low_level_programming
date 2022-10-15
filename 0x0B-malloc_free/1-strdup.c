c6c3935ec99251230ec24993937a0725e6f855da
CodeX1991 <oghenewonaholy@gmail.com>
2022-10-15 04:19:16 -0700

    Add a function that returns a newly allocated pace in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer to a newly allocated space in memory
+ * which contains a copy of the string gieven as a parameter.
+ * @str: pointer to the the given string
+ *
+ * Return: NULL if str = NULL or insufficient memory available
+ * On success - a pointer to the duplicated string.
+ *
+ */
+char *_strdup(char *str)
+{
+	unsigned int i, j, len = 0;
+	char *s;
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		len++;
+	}
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	s = malloc(sizeof(char) * len);
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		s[i] = str[i];
+	}
+	return (s);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,2 +1,3 @@
 Learning about malloc in c
 0-create_array.c: A function that creates an array of chars, and inintializes it with a specific char
+1-setrup.c: function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
