96bbdd178d51da2f03d262a4780df46e6ebde1b0
CodeX1991 <oghenewonaholy@gmail.com>
2023-01-01 06:04:52 -0800

    Write 6-cap_string.c that capitalize the initiates of words in a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize all words of a string
+ * @s: pointer to the first element of the array
+ *
+ * Return: the string of the capitalize words
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char p[] = {32, '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if ((s[i] == 0 || s[i - 1] == p[j]) && (s[i] >= 97 && s[i] <= 122))
+				s[i] -= 32;
+		}
+	}
+
+	return (s);
+}
