49e1b2be43f62783185992955cd4c3ed8161e9d7
CodeX1991 <oghenewonaholy@gmail.com>
2023-06-09 16:43:29 -0700

    Fix 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,13 +11,13 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc == 1)
+	if (argc == 1 || argc > 3)
 	{
 		printf("Error");
 		printf("\n");
 		return (1);
 	}
-	else
+	if (argc == 3)
 		printf("%d", atoi(argv[1]) * atoi(argv[2]));
 	printf("\n");
 	return (0);
