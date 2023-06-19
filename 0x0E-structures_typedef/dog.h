0a435c3e996c092ca5ac2232168f0b826cf3feac
CodeX1991 <oghenewonaholy@gmail.com>
2023-06-19 16:06:23 -0700

    Add a dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure of a dog
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: defining the structure of dog which tells the
+ * features of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
