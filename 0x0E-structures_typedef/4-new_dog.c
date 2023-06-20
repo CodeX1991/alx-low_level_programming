ce96cefe95b84e1188888b50c0f3371dea8e7ab6
CodeX1991 <oghenewonaholy@gmail.com>
2023-06-19 19:01:11 -0700

    Fixing the memory allocation issue

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,13 +15,73 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *user;
 
-	user = malloc(sizeof(dog_t));
+	if (name != NULL && owner != NULL)
+	{
+		int name_len = _strlen_recursion(name);
+		int owner_len = _strlen_recursion(owner);
 
-	if (user == NULL)
-		return (NULL);
-	user->name = name;
-	user->age = age;
-	user->owner = owner;
+		user = malloc(sizeof(dog_t));
+
+		if (user == NULL)
+			return (NULL);
+
+		user->name = malloc(sizeof(int) * name_len);
+		if (user->name == NULL)
+		{
+			free(user);
+			return (NULL);
+		}
+
+		user->owner = malloc(sizeof(char) * owner_len);
+		if (user->owner == NULL)
+		{
+			free(user->name);
+			free(user);
+			return (NULL);
+		}
+
+		user->name = _strcpy(user->name, name);
+		user->owner = _strcpy(user->owner, owner);
+		user->age = age;
+	}
 
 	return (user);
+}
+
+
+/**
+ * _strcpy - A function that copies the string pointed by src
+ * including the terminating null byte (\0), to the  buffer
+ * pointedd to by dest
+ * @dest: pointer to copy to
+ * @src: pointer to copy fromm
+ *
+ * Return: The value of the pointer, dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to the string to be counted
+ *
+ * Return: the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s == 0)
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
