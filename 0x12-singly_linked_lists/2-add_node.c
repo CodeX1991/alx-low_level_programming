47eb74bf9995729e79fcd04cc77f3d4d619e66ad
CodeX1991 <oghenewonaholy@gmail.com>
2023-07-03 16:07:26 -0700

    Add a function that adds a new node at the beginning of a list_t list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node - prints all the len=ments of a list_t list
+ * @head: double pointer to the struct created
+ * @str: string to add
+ * Return: the number of node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	if (head != NULL && str != NULL)
+	{
+		new = malloc(sizeof(list_t));
+
+		if (new == NULL)
+			return (NULL);
+
+		new->str = strdup(str);
+		new->len = _strlen_recursion(str);
+		new->next = *head;
+		*head = new;
+
+		return (*head);
+	}
+	return (NULL);
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to the string to be counted
+ *
+ * Return: the length of the string
+ */
+int _strlen_recursion(const char *s)
+{
+
+	if (*s == 0)
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
