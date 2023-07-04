c6750467bde5d1c0ead3fa0736a4f50aa696f86c
CodeX1991 <oghenewonaholy@gmail.com>
2023-07-03 17:59:35 -0700

    Add a 64-bit program in assembly that prints Hello, Holberton, followed by a new line

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -6,11 +6,11 @@ section .text
 global main
 main:
         mov     rax, 1          ;sytem call number should be stored in rax
-        mov     rdi, 2          ;system #1 in rdi: where to write (descriptor)
+        mov     rdi, 1          ;system #1 in rdi: where to write (descriptor)
         mov     rsi, message    ; argument #2 in rsi: where does the string start?
         mov     rdx, messagelen         ; argument #3 in rdx: how many bytes to write?
         syscall                 ; this instruction invokes a system call
 
 	mov rax, 60		; 'exit' syscall number
-	xor rdi, rdi
+	mov rdi, 0
 	syscall
