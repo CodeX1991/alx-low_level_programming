4cce93f7eb4576268f766566962b0bfff8dece95
CodeX1991 <oghenewonaholy@gmail.com>
2023-07-11 18:57:38 -0700

    Add a function that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: to write from
+ * @letters: number of char to write
+ *
+ * Return: letters on success, otherwise 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t n;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char *) * letters);
+	if (buffer == NULL)
+		return (0);
+	n = read(fd, buffer, letters);
+
+	if (n == -1)
+		return (0);
+	write(1, buffer, n);
+	free(buffer);
+	close(fd);
+
+	return (n);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+Learning file descriptor with Alx
