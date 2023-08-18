655fd5e8bf0d4bea8fa624399d36daa730c5fb04
CodeX1991 <oghenewonaholy@gmail.com>
2023-08-18 03:26:19 -0700

    Fix prev address error

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,17 +12,23 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = NULL;
 
-	if (head)
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (*head)
 	{
-		new = malloc(sizeof(dlistint_t));
-		if (new == NULL)
-			return (NULL);
-
-		new->n = n;
 		new->next = *head;
+		new->prev = (*head)->prev;
+		(*head)->prev = new;
 		*head = new;
-
 		return (*head);
 	}
-	return (NULL);
+
+	new->next = *head;
+	new->prev = NULL;
+	*head = new;
+	return (*head);
 }
