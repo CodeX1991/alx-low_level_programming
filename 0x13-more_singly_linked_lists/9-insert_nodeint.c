c055187846be48064348d330659089f8641979a8
CodeX1991 <oghenewonaholy@gmail.com>
2023-08-20 04:40:28 -0700

    Fix special cases

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,11 +17,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
+	new_node->n = n;
+	current_node = *head;
 
 	if (*head != NULL)
 	{
-		new_node->n = n;
-		current_node = *head;
+
 		if (idx > 0)
 		{
 			while (current_node != NULL)
@@ -40,12 +41,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			if (idx > index)
 				return (NULL);
 		}
-		else
-		{
-			new_node->next = *head;
-			*head = new_node;
-			return (*head);
-		}
+	}
+	if (idx == 0 && *head == NULL)
+	{
+		new_node->next = current_node;
+		*head = new_node;
+		return (new_node);
 	}
 	return (NULL);
 }
