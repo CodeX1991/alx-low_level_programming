1225660958d75a183de8657014de4a043747096c
CodeX1991 <oghenewonaholy@gmail.com>
2023-08-20 05:01:15 -0700

    Fixing idx > index and idx == 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,42 +11,33 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node = NULL, *pre_node = NULL, *current_node = NULL;
+	listint_t *new_node = NULL, *current_node = NULL;
 	unsigned int index = 0;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = n;
 	current_node = *head;
 
-	if (*head != NULL)
-	{
-
-		if (idx > 0)
-		{
-			while (current_node != NULL)
-			{
-				if (index == idx)
-				{
-					pre_node->next = new_node;
-					new_node->next = current_node;
-					current_node = new_node;
-					return (current_node);
-				}
-				pre_node = current_node;
-				current_node = current_node->next;
-				index++;
-			}
-			if (idx > index)
-				return (NULL);
-		}
-	}
-	if (idx == 0 && *head == NULL)
+	if (idx == 0)
 	{
 		new_node->next = current_node;
 		*head = new_node;
 		return (new_node);
 	}
-	return (NULL);
+
+	while (index < (idx - 1))
+	{
+		if (current_node == NULL || current_node->next == NULL)
+			return (NULL);
+		current_node = current_node->next;
+		index++;
+	}
+
+	new_node->next = current_node->next;
+	current_node->next = new_node;
+
+	return (new_node);
 }
