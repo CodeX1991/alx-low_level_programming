878c1ee7cbf555c20dd01dd9465c6429d29c7e21
CodeX1991 <oghenewonaholy@gmail.com>
2023-08-21 11:10:12 -0700

    Fix the head pointint to NULL error

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *current_node = NULL, *prev_node = NULL, *temp = NULL;
 	unsigned int idx = 0;
 
-	if (head && index > 0)
+	if (*head && index > 0)
 	{
 		current_node = *head;
 		while (current_node != NULL)
@@ -31,7 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			idx++;
 		}
 	}
-	if (head && index == 0)
+	if (*head && index == 0)
 	{
 		current_node = *head;
 		*head = (*head)->next;
